7726b00dea8985290b06f8773274b1b6473b00e7
FernirTeam <team@fernir.co>
2024-10-12 23:58:34 +0300

    move filters to component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -10,29 +10,31 @@ import Link from "next/link"
 export const Header = () => {
   return (
     <NavigationMenu className="text-primary-foreground uppercase font-bold">
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <Link href="/">
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Home
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/races">
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Races
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/spells">
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Spells
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-      </NavigationMenuList>
+      <nav>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href="/">
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Home
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/races">
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Races
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/spells">
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Spells
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </nav>
     </NavigationMenu>
   )
 }
